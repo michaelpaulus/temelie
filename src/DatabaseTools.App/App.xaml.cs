60120381e76e95c4a18f9eab1073c150384af771
Michael Paulus <mpaulus@n2y.com>
2021-12-06 13:44:51 -0500

    added package for databasetools.ui

--- src/DatabaseTools.App/App.xaml.cs
+++ src/DatabaseTools.App/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace DatabaseTools.UI
+namespace DatabaseTools.App
 {
     /// <summary>
     /// Interaction logic for App.xaml
---
+++ src/DatabaseTools.UI/publish.cmd
@@ -0,0 +1,1 @@
+dotnet nuget push bin\Release\DatabaseTools.UI.1.0.26.nupkg --source "https://n2y.pkgs.visualstudio.com/_packaging/n2y/nuget/v3/index.json" --api-key az
