1c312842d15dc4377181793feac965618b521727
Michael Paulus <mpaulus@n2y.com>
2023-05-09 15:33:18 -0400

    updated to 2.0.0

--- src/DatabaseTools/publish.cmd
+++ src/DatabaseTools/publish.cmd
@@ -1,1 +1,1 @@
-dotnet nuget push bin\Release\DatabaseTools.1.0.34.nupkg --source "https://n2y.pkgs.visualstudio.com/_packaging/n2y/nuget/v3/index.json" --api-key az
+dotnet nuget push bin\Release\DatabaseTools.2.0.0.nupkg --source "https://n2y.pkgs.visualstudio.com/_packaging/n2y/nuget/v3/index.json" --api-key az
