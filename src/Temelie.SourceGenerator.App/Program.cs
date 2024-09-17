4901f0bcc4e17d4bbc915ca1a15c93f5c5a4a6b2
michael.paulus <michael.paulus@icevonline.com>
2024-09-17 10:18:45 -0400

    update generator

--- src/Temelie.SourceGenerator.App/Program.cs
+++ src/Temelie.SourceGenerator.App/Program.cs
@@ -1,8 +1,8 @@
 using Temelie.SourceGenerator;
 using Temelie.SourceGenerator.Configuration;
 
-var fileGenerator = new FileGenerator(); 
+var fileGenerator = new FileGenerator();
 
 await fileGenerator.GenerateEntitiesAsync(Path.Combine(DirectoryConfig.RepoDirectory, "Temelie.sln"), "AdventureWorks.Database", "AdventureWorks.Entities");
-await fileGenerator.GenerateEntityFrameworkCoreAsync(Path.Combine(DirectoryConfig.RepoDirectory, "Temelie.sln"), "AdventureWorks.Entities", "AdventureWorks.Server.Repository");
-await fileGenerator.GenerateRepositoryAsync(Path.Combine(DirectoryConfig.RepoDirectory, "Temelie.sln"), "AdventureWorks.Entities", "AdventureWorks.Server.Repository");
+await fileGenerator.GenerateEntityFrameworkCoreAsync(Path.Combine(DirectoryConfig.RepoDirectory, "Temelie.sln"), "AdventureWorks.Entities", "AdventureWorks.Server.Repository", Path.Combine("_Generated", "EntityFrameworkCore"));
+await fileGenerator.GenerateRepositoryAsync(Path.Combine(DirectoryConfig.RepoDirectory, "Temelie.sln"), "AdventureWorks.Entities", "AdventureWorks.Server.Repository", Path.Combine("_Generated", "Repository"));
